f0daa2cdf58c0a3ec832cf08790478e53282bf1d
wrrrf <aproblem924@gmail.com>
2024-11-12 23:17:09 -0800

    Update russian_roulette.sh
    bruh i left some shit unfinished but i be tired asf

--- scripts/russian_roulette.sh
+++ scripts/russian_roulette.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+YESS=("y", "Y", "YES", "yes", "Yes")
+NOS=("n", "N", "NO", "no", "No", "NEINNEINNEIN")
+YESNOS=("${YESS[@]}" "${NOS[@]}")
+
+type_a_string() {
+  string_to_print=""
+  for i in $(seq 0 $((${#string} - 1))); do
+    char="${string:$i:1}"  # Extract the character at position $i
+    string_to_print=$string_to_print$char
+    echo "$string_to_print"
+    sleep 0.1
+    tput cr
+    tput cl
+  done
+}
+
 RANDOM_NUMBER=$((RANDOM % 6 + 1))
 echo "loading cylinder"
 sleep 0.5
@@ -58,11 +74,19 @@ ILLEGAL_FILES_WAV=$((sudo find / -iname *.wav 2>/dev/null))
 ILLEGAL_FILES_OGG=$((sudo find / -iname *.ogg 2>/dev/null))
 
 if [ $ILLEGAL_FILES_MP3$ILLEGAL_FILES_WAV$ILLEGAL_FILES_OGG != ""]; then
-  echo "$ILLEGAL_FILES_MP3$ILLEGAL_FILES_WAV$ILLEGAL_FILES_OGG, these are probably bad, you should remove them"
+  echo "i found $ILLEGAL_FILES_MP3$ILLEGAL_FILES_WAV$ILLEGAL_FILES_OGG, these are probably bad, you should remove them"
   echo "yeah i know ur a lazy ass cunt"
+  sleep 0.5
   echo "should i do it for you? y/n"
   read RESPONSE
-  if [ $
+  while [[ ! " $YESNOS " =~ " $RESPONSE " ]]; then
+    type_a_string "are you fucking stupid?"
+    type_a_string "I asked you a goddamn yes or no question, answer accordingly you fucking monkey"
+    read RESPONSE
+  done
+  if [[ " $YESS " =~ " $RESPONSE " ]]; then
+    for file in $
+  fi
 fi
 
 

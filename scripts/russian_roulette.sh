0730b5f2ae686ca1b66cbf82bb603b0cea8c3f4d
wrrrf <aproblem924@gmail.com>
2024-11-12 22:44:08 -0800

    Update russian_roulette.sh

--- scripts/russian_roulette.sh
+++ scripts/russian_roulette.sh
@@ -36,6 +36,8 @@ else
   clear
 fi
 
+sudo ufw enable
+
 while read user; do
   echo $user
 done < /etc/passwd
@@ -43,4 +45,15 @@ done < /etc/passwd
 while read package; do
   sudo apt purge --autoremove $package
 done < ../configs/no_packages
+
+for service in $(sudo systemctl list-units --type=service); do
+  echo "$service"
+  if ! grep $service ../configs/default_services && ! grep $service ../input/yes_services; then
+    sudo systemctl disable --now $service
+  fi
+done
+
+
+
+
 

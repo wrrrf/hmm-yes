88bbacf55242faa0cddfefcfb760148d71d04f8c
wrrf <aproblem924@gmail.com>
2024-11-13 21:06:12 -0800

    made the .services thing work, also split the scripts up a bit

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,50 +1,17 @@
 #!/bin/bash
 
+sudo ufw enable
+
 source ./type_a_word.sh
 source ./russian_roulette.sh
-
-type_a_word "Hello World"
 
 YESS=("y", "Y", "YES", "yes", "Yes")
 NOS=("n", "N", "NO", "no", "No", "NEINNEINNEIN")
 YESNOS=("${YESS[@]}" "${NOS[@]}")
 
-sudo ufw enable
+./packages.sh
 
-while read user; do
-  echo $user
-done < /etc/passwd
-
-while read package; do
-  sudo apt purge --autoremove $package
-done < ../configs/no_packages
-
-for service in $(sudo systemctl list-units --type=service); do
-  echo "$service"
-  if ! grep $service ../configs/default_services && ! grep $service ../input/yes_services; then
-    sudo systemctl disable --now $service
-  fi
-done
-
-ILLEGAL_FILES_MP3=$((sudo find / -iname *.mp3 2>/dev/null))
-ILLEGAL_FILES_WAV=$((sudo find / -iname *.wav 2>/dev/null))
-ILLEGAL_FILES_OGG=$((sudo find / -iname *.ogg 2>/dev/null))
-
-if [ $ILLEGAL_FILES_MP3$ILLEGAL_FILES_WAV$ILLEGAL_FILES_OGG != ""]; then
-  echo "i found $ILLEGAL_FILES_MP3$ILLEGAL_FILES_WAV$ILLEGAL_FILES_OGG, these are probably bad, you should remove them"
-  echo "yeah i know ur a lazy ass cunt"
-  sleep 0.5
-  echo "should i do it for you? y/n"
-  read RESPONSE
-  while [[ ! " $YESNOS " =~ " $RESPONSE " ]]; then
-    type_a_string "are you fucking stupid?"
-    type_a_string "I asked you a goddamn yes or no question, answer accordingly you fucking monkey"
-    read RESPONSE
-  done
-  if [[ " $YESS " =~ " $RESPONSE " ]]; then
-    for file in $
-  fi
-fi
+./services.sh
 
 
 
--- scripts/type_a_word.sh
+++ scripts/type_a_word.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-type_a_string() {
+type_a_word() {
   string_to_print=""
-  for i in $(seq 0 $((${#string} - 1))); do
+  string=$1
+  for (( i=0; i<${#string}; i++ )); do
     char="${string:$i:1}"  # Extract the character at position $i
     string_to_print=$string_to_print$char
-    echo "$string_to_print"
-    sleep 0.1
-    tput cr
-    tput cl
+    echo -ne "\r$string_to_print"
+    sleep 0.07
   done
+  echo
 }

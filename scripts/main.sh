5a4adebeb4fc0302e89406670bc4cf00ce6ca2c5
wrrrf <aproblem924@gmail.com>
2024-11-13 17:32:13 -0800

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,18 +3,6 @@ YESS=("y", "Y", "YES", "yes", "Yes")
 NOS=("n", "N", "NO", "no", "No", "NEINNEINNEIN")
 YESNOS=("${YESS[@]}" "${NOS[@]}")
 
-type_a_string() {
-  string_to_print=""
-  for i in $(seq 0 $((${#string} - 1))); do
-    char="${string:$i:1}"  # Extract the character at position $i
-    string_to_print=$string_to_print$char
-    echo "$string_to_print"
-    sleep 0.1
-    tput cr
-    tput cl
-  done
-}
-
 sudo ufw enable
 
 while read user; do

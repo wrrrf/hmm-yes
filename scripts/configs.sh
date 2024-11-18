8a06c08c40fef981e142cea42c452248781f82a5
wrrf <aproblem924@gmail.com>
2024-11-17 19:49:47 -0800

    now sets permissions for other configs

--- scripts/configs.sh
+++ scripts/configs.sh
@@ -1,5 +1,8 @@
 cp ../sysconf/common-auth /etc/pam.d/common-auth
+sudo chmod 640 /etc/pam.d/common-auth
 cp ../sysconf/common-password /etc/pam.d/common-password
+sudo chmod 640 /etc/pam.d/common-password
 cp ../sysconf/login.defs /etc/pam.d/login.defs
+sudo chmod 640 /etc/login.defs
 cp ../sysconf/sudoers /etc/sudoers
 sudo chmod 440 /etc/sudoers

d04aa0b07fa74f45fa67875d2dc3a1c976b31a54
wrrf <aproblem924@gmail.com>
2024-11-17 19:48:57 -0800

    now properly sets permissions for sudoers

---
+++ scripts/configs.sh
@@ -0,0 +1,5 @@
+cp ../sysconf/common-auth /etc/pam.d/common-auth
+cp ../sysconf/common-password /etc/pam.d/common-password
+cp ../sysconf/login.defs /etc/pam.d/login.defs
+cp ../sysconf/sudoers /etc/sudoers
+sudo chmod 440 /etc/sudoers
